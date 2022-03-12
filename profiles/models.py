cad8bd6c2a62de4f9e9f4879036bc4fde775d673
hatredholder <the.great.justice@bk.ru>
2022-03-12 16:03:30 +0300

    first commit

---
+++ profiles/models.py
@@ -0,0 +1,34 @@
+from .utils import get_random_code
+from django.db import models
+from django.contrib.auth.models import User
+from django.template.defaultfilters import slugify
+
+
+class Profile(models.Model):
+    first_name = models.CharField(max_length=200, blank=True)
+    last_name = models.CharField(max_length=200, blank=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(default='No Bio..', max_length=300)
+    email = models.EmailField(max_length=200, blank=True)
+    country = models.CharField(max_length=200, blank=True)
+    avatar = models.ImageField(default='avatar.png', upload_to='avatars/')
+    friends = models.ManyToManyField(User, blank=True, related_name='friends')
+    slug = models.SlugField(unique=True, blank=True)
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username}-{self.created}"
+    
+    def save(self, *args, **kwargs):
+        ex = False
+        if self.first_name and self.last_name:
+            to_slug = slugify(str(self.first_name) + ' ' + str(self.last_name))
+            ex = Profile.objects.filter(slug=to_slug).exists()
+            while ex:
+                to_slug = slugify(to_slug + " " + str(get_random_code()))
+                ex = Profile.objects.filter(slug=to_slug).exists()
+        else:
+            to_slug = str(self.user)
+        self.slug = to_slug
+        super().save(*args, **kwargs)
