019611c837220d23f45425f7083ca97d571e4007
hatredholder <the.great.justice@bk.ru>
2022-03-12 16:49:38 +0300

    added a relationship model, added a create profile signal, added add to friends signal

--- profiles/models.py
+++ profiles/models.py
@@ -18,7 +18,7 @@ class Profile(models.Model):
     created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"{self.user.username}-{self.created}"
+        return f"{self.user.username}-{self.created.strftime('%m-%d-%Y')}"
     
     def save(self, *args, **kwargs):
         ex = False
@@ -32,3 +32,18 @@ class Profile(models.Model):
             to_slug = str(self.user)
         self.slug = to_slug
         super().save(*args, **kwargs)
+
+STATUS_CHOICES = (
+    ('sent', 'sent'),
+    ('accepted', 'accepted')
+)
+
+class Relationship(models.Model):
+    sender = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='sender')
+    receiver = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='receiver')
+    status = models.CharField(max_length=8, choices=STATUS_CHOICES)
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return  f"{self.sender}-{self.receiver}-{self.status}"
