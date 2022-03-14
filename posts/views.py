d6f1600b2504401e111b3c69751aa147d73ef082
hatredholder <the.great.justice@bk.ru>
2022-03-14 19:43:30 +0300

    added comment functionality, post add functionality

--- posts/views.py
+++ posts/views.py
@@ -1,14 +1,43 @@
-from django.shortcuts import redirect, render
 from .models import Post, Like
+from .forms import PostModelForm, CommentModelForm
+from django.shortcuts import redirect, render
 from profiles.models import Profile
 
 
 def post_comment_create_and_list_view(request):
     qs = Post.objects.all()
     profile = Profile.objects.get(user=request.user)
+
+    p_form = PostModelForm()
+    c_form = CommentModelForm()
+    post_added = False
+
+    profile = Profile.objects.get(user=request.user)
+
+    if 'submit_p_form' in request.POST:
+
+        p_form = PostModelForm(request.POST or None, request.FILES)
+        if p_form.is_valid():
+            instance = p_form.save(commit=False)
+            instance.author = profile
+            instance.save()
+            p_form = PostModelForm()
+            post_added = True
+
+    if 'submit_c_form' in request.POST:
+        c_form = CommentModelForm(request.POST)
+        if c_form.is_valid():
+            instance = c_form.save(commit=False)
+            instance.user = profile
+            instance.post = Post.objects.get(id=request.POST.get('post_id'))
+            instance.save()
+            c_form = CommentModelForm()
     context = {
         'qs':qs,
         'profile':profile,
+        'p_form':p_form,
+        'c_form':c_form,
+        'post_added':post_added
     }
     return render(request, 'posts/main.html', context)
 
