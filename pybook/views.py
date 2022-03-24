e9c9962adc1c42eec4a619fcc061704972793dad
hatredholder <the.great.justice@bk.ru>
2022-03-24 13:23:57 +0300

    added a welcome page for non authorized users, changed navbar

--- pybook/views.py
+++ pybook/views.py
@@ -2,14 +2,11 @@
 
 
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.contrib.auth.decorators import login_required
 
 
-@login_required
 def home_view(request):
-    user = request.user
-    context = {
-        'user':user,
-    }
-    return render(request, 'main/home.html', context)
+    if request.user.is_authenticated:
+        return redirect('posts/')
+    return render(request, 'main/home.html')
