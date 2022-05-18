fbd1bcc73acc2e689de7890d44e9d759880f9537
hatredholder <the.great.justice@bk.ru>
2022-05-18 19:41:57 +0300

    reformatted code

--- pybook/settings.py
+++ pybook/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # Added Apps
+    # Apps
     'profiles',
     'posts',
     # django all-auth
--- pybook/views.py
+++ pybook/views.py
@@ -1,9 +1,4 @@
-
-
-
-from django.http import HttpResponse
 from django.shortcuts import redirect, render
-from django.contrib.auth.decorators import login_required
 
 
 def home_view(request):
