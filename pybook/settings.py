423fca9e92f9f500d1263f23b02270b63d88c0de
hatredholder <the.great.justice@bk.ru>
2022-06-19 16:44:54 +0300

    added whitenoise middleware

--- pybook/settings.py
+++ pybook/settings.py
@@ -50,6 +50,9 @@ if DEBUG:
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
