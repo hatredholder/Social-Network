3000413a4e1717e4c17ef2f59b53c76c15346edf
hatredholder <the.great.justice@bk.ru>
2022-07-30 17:16:07 +0300

    commented code

--- pybook/views.py
+++ pybook/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import redirect, render
 
 
 def home_view(request):
+    """
+    Welcoming page view, redirects to the board if user is authenticated.
+    """
     if request.user.is_authenticated:
         return redirect('posts/')
     return render(request, 'main/home.html')
