82b851f7fa46099078858ea60280e43de9749e12
hatredholder <the.great.justice@bk.ru>
2022-08-18 16:45:58 +0300

    reformatted code

--- profiles/urls.py
+++ profiles/urls.py
@@ -13,12 +13,14 @@ urlpatterns = [
     path('users/<slug>/', ProfileDetailView.as_view(), name='profile-detail-view'),
     path('messenger/', MessengerListView.as_view(), name='messenger-list-view'),
     path('chat/<pk>/', ChatMessageView.as_view(), name='chat-message-view'),
+
     path('myprofile/', my_profile_view, name='my-profile-view'),
     path('search/', search_profiles, name='search-profiles-view'),
     path('my_friends/', my_friends_view, name='my-friends-view'),
     path('received_invites/', received_invites_view, name='received-invites-view'),
     path('sent_invites/', sent_invites_view, name='sent-invites-view'),
     path('switch_follow/', switch_follow_user, name='switch-follow-view'),
+    
     path('send-invite/', send_invitation, name='send-invite'),
     path('remove-friend/', remove_friend, name='remove-friend'),
     path('received_invites/accept/', accept_invitation, name='accept-invite'),
