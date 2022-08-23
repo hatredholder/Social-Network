ac1390a4046e1b1e8b6f1dd4ade187eccf54b296
hatredholder <the.great.justice@bk.ru>
2022-08-23 19:42:25 +0300

    updated imports in admin.py and views.py

--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,7 @@
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.http import HttpResponseRedirect
 from django.views.generic import DeleteView, UpdateView
