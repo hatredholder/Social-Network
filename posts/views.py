da15a16001f238f23e38fa0b4f5050852ac701ef
hatredholder <the.great.justice@bk.ru>
2022-08-24 19:57:36 +0300

    sorted imports, finished working with posts views

--- posts/views.py
+++ posts/views.py
@@ -1,9 +1,9 @@
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.http import HttpResponseRedirect
 from django.views.generic import DeleteView, UpdateView
 from profiles.views_utils import get_request_user_profile, redirect_back
 
