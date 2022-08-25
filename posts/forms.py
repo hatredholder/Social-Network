89c4b06603027e271f847547d695e673b44745cb
hatredholder <the.great.justice@bk.ru>
2022-08-25 18:48:54 +0300

    reformatted code

--- posts/forms.py
+++ posts/forms.py
@@ -5,18 +5,21 @@ from .models import Comment, Post
 
 class PostCreateModelForm(forms.ModelForm):
     content = forms.CharField(widget=forms.Textarea(attrs={'rows':2}))
+    
     class Meta:
         model = Post
         fields = ('content', 'image', )
 
 class PostUpdateModelForm(forms.ModelForm):
     content = forms.CharField(widget=forms.Textarea(attrs={'rows':2}))
+
     class Meta:
         model = Post
         fields = ('content', )
 
 class CommentCreateModelForm(forms.ModelForm):
-    body = forms.CharField(label='', widget=forms.TextInput(attrs={'placeholder':'Add a comment..'}))
+    body = forms.CharField(widget=forms.TextInput(attrs={'placeholder':'Add a comment..'}))
+
     class Meta:
         model = Comment
         fields = ('body', )
