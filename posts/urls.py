e47b7b123b38f5346357ab12157e1d657d71f1d6
hatredholder <rattlingserpent@gmail.com>
2022-10-25 18:42:30 +0300

    formatted code with black

--- posts/urls.py
+++ posts/urls.py
@@ -1,15 +1,19 @@
 from django.urls import path
 
-from .views import (CommentDeleteView, PostDeleteView, PostUpdateView,
-                    post_comment_create_and_list_view, switch_like)
+from .views import (
+    CommentDeleteView,
+    PostDeleteView,
+    PostUpdateView,
+    post_comment_create_and_list_view,
+    switch_like,
+)
 
-app_name = 'posts'
+app_name = "posts"
 
 urlpatterns = [
-    path('', post_comment_create_and_list_view, name='main-post-view'),
-    path('like/', switch_like, name='switch-like-view'),
-
-    path('<pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
-    path('<pk>/update/', PostUpdateView.as_view(), name='post-update'),
-    path('comments/<pk>/delete/', CommentDeleteView.as_view(), name='comment-delete'),
+    path("", post_comment_create_and_list_view, name="main-post-view"),
+    path("like/", switch_like, name="switch-like-view"),
+    path("<pk>/delete/", PostDeleteView.as_view(), name="post-delete"),
+    path("<pk>/update/", PostUpdateView.as_view(), name="post-update"),
+    path("comments/<pk>/delete/", CommentDeleteView.as_view(), name="comment-delete"),
 ]
